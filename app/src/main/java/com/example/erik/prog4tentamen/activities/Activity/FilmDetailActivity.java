0a37e113ee235c011a281b144882fcbf46867936
teun2408 <teun2408@gmail.com>
2017-06-17 19:28:57 +0200

    Revert "Feature/films zoeken naam"

--- app/src/main/java/com/example/erik/prog4tentamen/activities/Activity/FilmDetailActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.erik.prog4tentamen.activities.Activity;
-
-/**
- * Created by Erik on 15-6-2017.
- */
-
-public class FilmDetailActivity {
-}
--- app/src/main/java/com/example/erik/prog4tentamen/controller/apiController.java
+++
@@ -1,9 +0,0 @@
-package com.example.erik.prog4tentamen.controller;
-
-/**
- * Created by Teunvz on 13-6-2017.
- */
-
-public class apiController {
-
-}
